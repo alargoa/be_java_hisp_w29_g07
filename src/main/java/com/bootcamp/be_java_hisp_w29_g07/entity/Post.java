a75479f1093e30055ad621b7d2ddce3de973ed1b
Andres Largo <andres.largo@mercadolibre.com.co>
2025-01-22 11:06:54 -0500

    Add posts.json and implement method to load posts

--- src/main/java/com/bootcamp/be_java_hisp_w29_g07/entity/Post.java
+++ src/main/java/com/bootcamp/be_java_hisp_w29_g07/entity/Post.java
@@ -1,5 +1,6 @@
 package com.bootcamp.be_java_hisp_w29_g07.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,7 +13,10 @@ import java.time.LocalDate;
 public class Post {
     private Integer id;
     private Integer userId;
+
+    @JsonFormat(pattern = "dd-MM-yyyy")
     private LocalDate date;
+
     private Product product;
     private Category category;
     private Double price;
