5ac4ed04b5f94bbe47c21b3cc39c52ae29390ef4
Andres Largo <andres.largo@mercadolibre.com.co>
2025-01-22 10:00:46 -0500

    Add users.json and implement method to load users

--- be_java_hisp_w29_g07/src/main/java/com/bootcamp/be_java_hisp_w29_g07/repository/PostRepositoryImpl.java
+++ be_java_hisp_w29_g07/src/main/java/com/bootcamp/be_java_hisp_w29_g07/repository/PostRepositoryImpl.java
@@ -1,12 +1,10 @@
 package com.bootcamp.be_java_hisp_w29_g07.repository;
 
 import com.bootcamp.be_java_hisp_w29_g07.entity.Post;
-import com.bootcamp.be_java_hisp_w29_g07.entity.User;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public class PostRepositoryImpl implements  IPostRepository{
--- be_java_hisp_w29_g07/src/main/java/com/bootcamp/be_java_hisp_w29_g07/repository/UserRepositoryImpl.java
+++ be_java_hisp_w29_g07/src/main/java/com/bootcamp/be_java_hisp_w29_g07/repository/UserRepositoryImpl.java
@@ -1,8 +1,13 @@
 package com.bootcamp.be_java_hisp_w29_g07.repository;
 
 import com.bootcamp.be_java_hisp_w29_g07.entity.User;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.ResourceUtils;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -11,13 +16,24 @@ import java.util.Optional;
 public class UserRepositoryImpl implements IUserRepository{
     private List<User> users = new ArrayList<>();
 
-    public UserRepositoryImpl(List<User> users) {
-        // TO-DO: Build json file
-        this.users = users;
+    public UserRepositoryImpl() throws IOException {
+        loadUsersJson();
     }
 
     @Override
     public Optional<User> getUserById(Integer userId) {
         return users.stream().filter(u -> u.getId().equals(userId)).findFirst();
+    }
+
+    private void loadUsersJson() throws IOException {
+        File file;
+        ObjectMapper objectMapper = new ObjectMapper();
+        List<User> usersJson;
+
+        file = ResourceUtils.getFile("classpath:users.json");
+        usersJson = objectMapper.readValue(file, new TypeReference<List<User>>() {
+        });
+
+        users = usersJson;
     }
 }
