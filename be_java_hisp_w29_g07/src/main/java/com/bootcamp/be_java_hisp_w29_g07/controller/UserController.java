21c6ab435684db45fe8ef2ab9412c824ec14addb
Catalina Morales Cárdenas <catalinamorales@javeriana.edu.co>
2025-01-22 09:17:25 -0500

    Creating lists in repository

--- be_java_hisp_w29_g07/src/main/java/com/bootcamp/be_java_hisp_w29_g07/controller/UserController.java
+++ be_java_hisp_w29_g07/src/main/java/com/bootcamp/be_java_hisp_w29_g07/controller/UserController.java
@@ -1,15 +1,19 @@
 package com.bootcamp.be_java_hisp_w29_g07.controller;
 
+import com.bootcamp.be_java_hisp_w29_g07.service.FollowServiceImpl;
 import com.bootcamp.be_java_hisp_w29_g07.service.UserServiceImpl;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
 
     private final UserServiceImpl userService;
+    private final FollowServiceImpl followService;
 
-    public UserController(UserServiceImpl userService){
+    public UserController(UserServiceImpl userService, FollowServiceImpl followService) {
         this.userService = userService;
+        this.followService = followService;
     }
-
 }
--- be_java_hisp_w29_g07/src/main/java/com/bootcamp/be_java_hisp_w29_g07/service/FollowServiceImpl.java
+++ be_java_hisp_w29_g07/src/main/java/com/bootcamp/be_java_hisp_w29_g07/service/FollowServiceImpl.java
@@ -1,7 +1,13 @@
 package com.bootcamp.be_java_hisp_w29_g07.service;
 
+import com.bootcamp.be_java_hisp_w29_g07.repository.FollowRepositoryImpl;
 import org.springframework.stereotype.Service;
 
 @Service
 public class FollowServiceImpl implements IFollowService{
+    private FollowRepositoryImpl followRepository;
+
+    public FollowServiceImpl(FollowRepositoryImpl followRepository) {
+        this.followRepository = followRepository;
+    }
 }
