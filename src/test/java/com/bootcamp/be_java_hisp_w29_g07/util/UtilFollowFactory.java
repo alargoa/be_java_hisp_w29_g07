289d89854b126edaa1629e984e2630bd6aa4faf8
bcamilos <camilo.sanchezpardo@mercadolibre.com.co>
2025-02-06 14:34:09 -0500

    add test for user follow seller

--- src/test/java/com/bootcamp/be_java_hisp_w29_g07/util/UtilFollowFactory.java
+++ src/test/java/com/bootcamp/be_java_hisp_w29_g07/util/UtilFollowFactory.java
@@ -1,4 +1,26 @@
 package com.bootcamp.be_java_hisp_w29_g07.util;
 
+import com.bootcamp.be_java_hisp_w29_g07.entity.Follow;
+import com.bootcamp.be_java_hisp_w29_g07.entity.User;
+
+import java.time.LocalDate;
+
+/**
+ * Factory class for creating {@link Follow} objects with predefined values.
+ * This class is used to simplify the creation of {@link Follow} instances, particularly
+ * in test scenarios where mock follow relationships are needed.
+ */
 public class UtilFollowFactory {
+
+    /**
+     * Creates a new instance of {@link Follow} with a predefined set of values.
+     * This method establishes a "follow" relationship between two {@link User} objects.
+     *
+     * @param user the {@link User} who is following another user.
+     * @param userToFollow the {@link User} who is being followed.
+     * @return a {@link Follow} object representing the follow relationship between the two users.
+     */
+    public static Follow getFollow(User user, User userToFollow) {
+        return new Follow(1, user, userToFollow, LocalDate.now());
+    }
 }
